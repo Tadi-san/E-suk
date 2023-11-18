39d7e1ac7463e82cd2900f75534e8e6d269befa6
Tadiwos Anegagregn <tadiwosanegagregn@gmail.com>
2023-11-18 13:17:57 +0300

    Update index.js
    client linked

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use('/uploads', express.static(__dirname + "/uploads"))
 app.use(express.json())
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173"
+    origin: "https://e-suk-2e5v.vercel.app"
 }))
 
 
