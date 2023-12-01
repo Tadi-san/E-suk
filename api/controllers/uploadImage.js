9aaa866e5f5d1560530c17d04e15f005e929751f
Tadi-san <tadiosanegagregn@yahoo.com>
2023-12-01 13:45:41 +0300

    ahhhhhhhhhhhhhhhhhhhh

--- api/controllers/uploadImage.js
+++ api/controllers/uploadImage.js
@@ -1,6 +1,6 @@
 const cloudinary = require('cloudinary').v2
 cloudinary.config({
-    cloud_name:process.env.cloud_name,
+    cloud_name:"de7rljdzr",
     api_key:process.env.api_key,
     api_secret:process.env.api_secret,
 
